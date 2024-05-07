2dcb17a27b90885bb9a4bc8c379a18649d18fdf1
Jorge Cerda <jorge_cerda_591@hotmail.com>
2024-05-07 16:03:05 -0400

    Añadiendo corsOptions, app.js

--- src/app.js
+++ src/app.js
@@ -12,12 +12,14 @@ let filesConfig = {
   responseOnLimit:
     "El peso del archivo que intentas subir supera el limite permitido.",
 };
+let corsOptions = {
+  origin: "*"};
 
 app.use(express.static("data"));
 app.use(expressFileUpload(filesConfig));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 //Rutas
 app.use("/apiV1", rutas);
