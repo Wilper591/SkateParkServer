4f9b4565ec95e6ecae653d620cefcc77e1d9efd2
Jorge Cerda <jorge_cerda_591@hotmail.com>
2024-08-22 18:15:12 -0400

    Actualizando config.js

--- src/db.js
+++ src/db.js
@@ -6,13 +6,7 @@ const config = {
   connectionString: process.env.URI_DB,
   ssl: process.env.SSL_DB === "true" ? { rejectUnauthorized: false } : false,
 };
-/* const config = {
-  user: "postgres",
-  host: "localhost",
-  database: "skatepark",
-  password: "wilper591",
-  port: 5432,
-}; */
+
 const pool = new Pool(config);
 
 pool
@@ -22,5 +16,5 @@ pool
     client.release(); // Libera el cliente después de la conexión
   })
   .catch((err) => console.error("Error de conexión", err.stack));
-  
+
 module.exports = { pool };
